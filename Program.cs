0649c93e600a4fe14422d1ef5d4ae6945455dd36
dmperezri <dmperezr@uamv.edu.ni>
2026-06-19 12:24:41 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Receta[] recetas = new Receta[50];
+
+int cantidad = 0;
+string perfilActual = "";
+int platosPreparados = 0;
+
+string rutaSaludables = "recetasSaludables.txt";
+string rutaAprendizaje = "recetasAprendizaje.txt";
+
+/* 
+    DANNIA:                                STEPHANIE
+Main()                              BuscarPorIngrediente()
+PantallaLogin()                     FiltrarPorTiempo()
+MostrarMenuSaludable()
+MostrarMenuAprendizaje()                    LUIS
+Banner()
+MostrarError()                      GuardarDatos()
+Pausar()                            CargarDatos()
+                                    LeerArchivo()CargarEjemplos()
+ PATRICIA                           Cargando()                          
+ AgregarReceta()                    ModuloSeguimiento()
+                                    MostrarRecetas()*/
+
+public struct Receta
+{
+    public string nombre;
+    public string ingredientes;
+    public string pasos;
+    public int tiempoPreparacion;
+    public string categoria;
+}
