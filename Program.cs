c2045586127f9827276ef1bf7e374a5ec910de8b
dmperezri <dmperezr@uamv.edu.ni>
2026-06-22 11:21:54 -0600

    agyegue login

--- Program.cs
+++ Program.cs
@@ -21,7 +21,42 @@ Pausar()                            CargarDatos()
  PATRICIA                           Cargando()                          
  AgregarReceta()                    ModuloSeguimiento()
                                     MostrarRecetas()*/
+void PantallaLogin()
+{
+    bool salirLogin = false;
+    while (!salirLogin)
+    {
+        Console.Clear();
+        Banner("BIENVENIDO A NUTRICOOK");
+        Console.WriteLine("  1. Perfil Saludable");
+        Console.WriteLine("  2. Perfil de Aprendizaje");
+        Console.WriteLine("  3. Salir\n");
+        Console.Write("  Seleccione su perfil: ");
 
+        string opc = Console.ReadLine()!;
+        if (opc == "1")
+        {
+            perfilActual = "Saludable";
+            MostrarMenuSaludable();
+        }
+        else if (opc == "2")
+        {
+            perfilActual = "Aprendizaje";
+            MostrarMenuAprendizaje();
+        }
+        else if (opc == "3")
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\nSaliendo de NutriCook. ¡Hasta pronto y buen provecho!");
+            Console.ResetColor();
+            salirLogin = true;
+        }
+        else
+        {
+            MostrarError("Opción no válida.");
+        }
+    }
+}
 public struct Receta
 {
     public string nombre;
