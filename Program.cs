cb870713508e607c9a51dbbb7c75b41d6aa43b20
dmperezri <dmperezr@uamv.edu.ni>
2026-06-22 11:24:37 -0600

    Agregue banner, mostrarerror, pausar

--- Program.cs
+++ Program.cs
@@ -132,6 +132,31 @@ void MostrarMenuAprendizaje()
 }
 
 
+
+void Banner(string titulo)
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine(new string('=', titulo.Length + 4));
+    Console.WriteLine($"  {titulo}");
+    Console.WriteLine(new string('=', titulo.Length + 4) + "\n");
+    Console.ResetColor();
+}
+
+void MostrarError(string msj)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"\n  [ERROR]: {msj}");
+    Console.ResetColor();
+    Pausar();
+}
+
+void Pausar()
+{
+    Console.WriteLine("\n  Presiona una tecla para continuar...");
+    Console.ReadKey();
+}
+
+
 public struct Receta
 {
     public string nombre;
