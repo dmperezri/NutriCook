e07582ab3eb980d66f4e86ba62a0dd36c4b7f6b1
dmperezri <dmperezr@uamv.edu.ni>
2026-06-24 09:40:28 -0600

    Agregando... guardar y cargar datos

--- Program.cs
+++ Program.cs
@@ -310,6 +310,47 @@ void ModuloDidactico()
     Pausar();
 }
 
+void GuardarDatos()
+{
+    try
+    {
+        using (StreamWriter swSaludable = new StreamWriter(rutaSaludables, false))
+        using (StreamWriter swAprendizaje = new StreamWriter(rutaAprendizaje, false))
+        {
+            for (int i = 0; i < cantidad; i++)
+            {
+                if (string.IsNullOrEmpty(recetas[i].nombre)) continue;
+
+                string linea = recetas[i].nombre + ";" + recetas[i].ingredientes + ";" + recetas[i].pasos + ";" + recetas[i].tiempoPreparacion;
+
+                if (recetas[i].categoria == "Saludable")
+                    swSaludable.WriteLine(linea);
+                else if (recetas[i].categoria == "Aprendizaje")
+                    swAprendizaje.WriteLine(linea);
+            }
+        }
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\n  ¡Registros guardados satisfactoriamente en archivos .txt!");
+        Console.ResetColor();
+    }
+    catch (Exception ex)
+    {
+        MostrarError("Hubo un problema al guardar: " + ex.Message);
+    }
+    Pausar();
+}
+
+void CargarDatos()
+{
+    cantidad = 0;
+    try
+    {
+        LeerArchivo(rutaSaludables, "Saludable");
+        LeerArchivo(rutaAprendizaje, "Aprendizaje");
+    }
+    catch (Exception) { }
+}
+
 public struct Receta
 {
     public string nombre;
